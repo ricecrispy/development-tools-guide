31c1d308a1945406daea5e8cc55c897337dad5bc
Jeffrey Siu <jeffythecashless@gmail.com>
2023-02-15 23:27:55 -0500

    fix: add optional firewall rule command

--- scripts/wsl-setup.ps1
+++ scripts/wsl-setup.ps1
@@ -6,3 +6,7 @@ wsl --install
 # If you like to use another OS, run wsl -l -o to find another distribution
 # wsl -l -o
 wsl --install -d Ubuntu
+
+# Depending on your firewall rule you may need to run the following command 
+# and add a firewall rule to allow wsl traffic 
+# New-NetFirewallRule -DisplayName "WSL" -Direction Inbound  -InterfaceAlias "vEthernet (WSL)"  -Action Allow
